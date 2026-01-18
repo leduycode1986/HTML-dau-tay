fd21818b3c4adaabe5b3fe3d250118f9e03267e2
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-01-18 18:57:41 +0700

    sua lai

--- shop-ban-hang/src/main.jsx
+++ shop-ban-hang/src/main.jsx
@@ -2,6 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 👇 1. BỔ SUNG DÒNG NÀY (Để nhập công cụ điều hướng)
+import { BrowserRouter } from 'react-router-dom' 
+
 createRoot(document.getElementById('root')).render(
-    <App />
+  // 👇 2. BỔ SUNG CẶP THẺ NÀY BAO QUANH <App />
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
 )
