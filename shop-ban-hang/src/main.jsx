f05b03bd835ac8f09661c499fbecef05882b8f74
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-01-18 19:05:04 +0700

    sua

--- shop-ban-hang/src/main.jsx
+++ shop-ban-hang/src/main.jsx
@@ -1,13 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+
+// 👇 1. NHẬP FILE GIAO DIỆN (Quan trọng để web đẹp)
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 👇 2. NHẬP BỘ DẪN ĐƯỜNG (Quan trọng để không lỗi Router)
 import { BrowserRouter } from 'react-router-dom'
 
-// 👇 QUAN TRỌNG: BỔ SUNG DÒNG NÀY ĐỂ WEB ĐẸP LẠI
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // Bọc App trong BrowserRouter thì mới chuyển trang được
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 )
