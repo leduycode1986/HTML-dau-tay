a57b8bace5b292c1e173228af45b48f6732f7818
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-01-18 14:15:54 +0700

    sua lai anh

--- shop-ban-hang/src/products.js
+++ shop-ban-hang/src/products.js
@@ -1,33 +1,32 @@
-// Đây là danh sách sản phẩm dùng chung cho toàn bộ website
 export const products = [
     {
       id: 1,
       ten: "Laptop Gaming",
       gia: "25.000.000 VNĐ",
-      anh: "https://images.unsplash.com/photo-1603302576837-37561b2e2302?auto=format&fit=crop&w=500&q=60"
+      anh: "/img/laptop.jpg" 
     },
     {
       id: 2,
       ten: "Điện thoại iPhone",
       gia: "30.000.000 VNĐ",
-      anh: "https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?auto=format&fit=crop&w=500&q=60"
+      anh: "/img/iphone.jpg"
     },
     {
         id: 3,
         ten: "Áo thun Coder",
         gia: "150.000 VNĐ",
-        anh: "https://plus.unsplash.com/premium_photo-1673356301535-21a5d5813124?auto=format&fit=crop&w=500&q=60"
+        anh: "/img/ao-thun.jpg"
     },
     {
         id: 4,
         ten: "Bàn phím cơ",
         gia: "500.000 VNĐ",
-        anh: "https://images.unsplash.com/photo-1587829741301-dc798b91add1?auto=format&fit=crop&w=500&q=60"
+        anh: "/img/ban-phim.jpg"
     },
     {
         id: 5,
         ten: "Tai nghe Bluetooth",
         gia: "850.000 VNĐ",
-        anh: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&w=500&q=60"
+        anh: "/img/tai-nghe.jpg"
     }
 ];
