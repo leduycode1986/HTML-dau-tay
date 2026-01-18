b08f40be2c4c442a46c2aa00b71fee0dcb66fc5c
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-01-18 16:00:43 +0700

    sua lai

--- shop-ban-hang/vite.config.js
+++ shop-ban-hang/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // QUAN TRỌNG: Dòng này giúp sửa lỗi đường dẫn khi vào trang con
+  base: '/', 
 })
