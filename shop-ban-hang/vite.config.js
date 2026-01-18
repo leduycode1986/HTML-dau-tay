054ebc4773d0c928e5d0e37e44c605624e118d77
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-01-18 19:39:44 +0700

    sua hoai

--- shop-ban-hang/vite.config.js
+++ shop-ban-hang/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // QUAN TRỌNG: Dòng này giúp sửa lỗi đường dẫn khi vào trang con
-  base: '/', 
+  server: {
+    hmr: {
+        overlay: false // Tắt báo lỗi che màn hình nếu có
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // Tăng giới hạn file build để không báo vàng
+  }
 })
