17123d87c84aaaa5386293a899039303389aa75c
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-01-26 22:47:02 +0700

    ss

--- shop-ban-hang/src/main.jsx
+++ shop-ban-hang/src/main.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
+import './style.css?v=2026';
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
