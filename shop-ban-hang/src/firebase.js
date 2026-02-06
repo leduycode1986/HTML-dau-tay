d946f2f5ba04667b36af9369b035bac5a8ea1a23
Le Khanh Duy <lekhanhduy1986@gmail.com>
2026-02-06 21:43:40 +0700

    sua ngay 05.2

--- shop-ban-hang/src/firebase.js
+++ shop-ban-hang/src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; // Thêm Storage (để up ảnh)
 import { getAuth } from "firebase/auth";       // Thêm Auth (để đăng nhập)
@@ -12,11 +12,17 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 // Khởi tạo các dịch vụ
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
+// Kiểm tra xem đã có kênh Admin chưa, nếu chưa thì tạo mới với cùng cấu hình
+const adminApp = !getApps().some(app => app.name === 'AdminChannel') 
+  ? initializeApp(firebaseConfig, 'AdminChannel') // Tên app riêng là 'AdminChannel'
+  : getApp('AdminChannel');
 
+// Xuất khẩu Auth và DB riêng cho Admin
+export const adminAuth = getAuth(adminApp);
+export const adminDb = getFirestore(adminApp);
 // Xuất ra để dùng ở các file khác
 export { db, auth, storage };
